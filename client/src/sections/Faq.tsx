9bff339ab13cfb51471cf85226ebe2add6e8b21c
Ruben Aguilar <ragui56@wgu.edu>
2026-04-23 23:35:20 -0700

    add faq temp data, basic map

--- client/src/sections/Faq.tsx
+++ client/src/sections/Faq.tsx
@@ -1,6 +1,41 @@
-const Faq= () => {
+
+
+const faqData = [
+  {
+    question: 'to be filled out',
+    answer: 'to be answered'
+  },
+  {
+    question: 'to be filled out',
+    answer: 'to be answered'
+  },
+  {
+    question: 'to be filled out',
+    answer: 'to be answered'
+  },
+  {
+    question: 'to be filled out',
+    answer: 'to be answered'
+  },
+  {
+    question: 'to be filled out',
+    answer: 'to be answered'
+  },
+]
+
+
+const Faq = () => {
   return (
-    <section>FAQ</section>
+    <section id='faq' className="py-32 overflow-hidden">
+      <div className="container mx-auto px-4">
+        {faqData.map(({ question, answer }, index) => (
+          <div key={index}>
+            {question}
+            {answer}
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
