a7a3f6bc9e683d661bccec24f8c5f0636309b722
Ruben Aguilar <ragui56@wgu.edu>
2026-05-08 21:35:16 -0700

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,12 @@
-import About from "./sections/About"
-import Contact from "./sections/Contact"
-import Faq from "./sections/Faq"
-import Footer from "./sections/Footer"
 import Header from "./sections/Header"
 import Hero from "./sections/Hero"
-import Reviews from "./sections/Reviews"
-import Services from "./sections/services/Services"
+
 
 const App = () => {
   return (
     <main className="min-h-screen overflow-hidden z-50 transition-all duration-300">
       <Header />
       <Hero />
-      <Services />
-      <About />
-      <Reviews />
-      <Faq />
-      <Contact />
-      <Footer />
     </main>
 
   )
