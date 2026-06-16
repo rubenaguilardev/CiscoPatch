a7fd42e9b70e2734f931da01c4a11824e202f1d6
Ruben Aguilar <ragui56@wgu.edu>
2026-06-15 18:05:03 -0700

    add grid container

--- client/src/sections/About.tsx
+++ client/src/sections/About.tsx
@@ -1,39 +1,14 @@
 const About = () => {
   return (
     <section className="w-full overflow-hidden py-32">
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
-      <h2>ABOUT</h2>
+      <div className="grid grid-cols-2">
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <h2>why choose use</h2>
+        </div>
+      </div>
     </section>
   )
 }
