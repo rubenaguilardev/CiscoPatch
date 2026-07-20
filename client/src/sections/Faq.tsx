87f017511ff577e7cc138396d919c953aa2a1dca
Ruben Aguilar <ragui56@wgu.edu>
2026-07-19 22:51:30 -0700

    add faq heading, some data

--- client/src/sections/Faq.tsx
+++ client/src/sections/Faq.tsx
@@ -1,8 +1,20 @@
-import { Section } from "lucide-react"
+const questions = [
+  {
+    question: "Do I need to be home for an estimate?",
+    answer: "No — just upload a few photos of the damaged area through our online form, and we'll get you an estimate without needing to schedule an in-person visit."
+  },
+  {
+
+  }
+]
+
 
 const Faq = () => {
   return (
-    <section id='faq' className="py-32 overflow-hidden w-full"></section>
+    <section id='faq' className="py-32 overflow-hidden w-full">
+      <h2>Frequently asked questions</h2>
+      <p>Have more questions? Reach out to our team or submit an estimation request.</p>
+    </section>
   )
 }
 
