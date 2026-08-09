bd1adb25667b1483477c1d6a68dc7c955246c35b
Ruben Aguilar <ragui56@wgu.edu>
2026-08-09 14:44:15 -0700

    create faq directory, data file

---
+++ client/src/sections/faq/Faq.tsx
@@ -0,0 +1,11 @@
+
+const Faq = () => {
+  return (
+    <section id='faq' className="py-32 overflow-hidden w-full">
+      <h2>Frequently asked questions</h2>
+      <p>Have more questions? Reach out to our team or submit an estimation request.</p>
+    </section>
+  )
+}
+
+export default Faq
