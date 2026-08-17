a23dec029c7e6ecf0fa0aa5a9c173a8b50adb39c
Ruben Aguilar <ragui56@wgu.edu>
2026-08-17 11:28:55 -0700

    faq update

--- client/src/sections/faq/Faq.tsx
+++ client/src/sections/faq/Faq.tsx
@@ -6,8 +6,8 @@ const Faq = () => {
     <section id='faq' className="py-32 overflow-hidden w-full ">
       <div className="container space-y-4 mx-auto px-4 md:px-6">
         <div className="text-center">
-          <h2 className="text-2xl xl:text-3xl mb-6 font-medium uppercase text-primary tracking-wide">Frequently asked questions</h2>
-          <p>Have more questions? Reach out to our team or submit an estimation request.</p>
+          <h2 className="text-2xl xl:text-3xl mb-4 font-medium uppercase text-primary tracking-wide leading-none">Frequently asked questions</h2>
+          <p className="text-lg font-medium leading-normal">Have more questions? Reach out to our team or submit an estimation request.</p>
         </div>
     
         {questions.map(({question, answer}, index) => (
