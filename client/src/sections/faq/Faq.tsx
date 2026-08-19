80483abf8f7993194d70c9a7045a0d183d39720d
Ruben Aguilar <ragui56@wgu.edu>
2026-08-19 00:14:30 -0700

    update

--- client/src/sections/faq/Faq.tsx
+++ client/src/sections/faq/Faq.tsx
@@ -1,12 +1,11 @@
-import Accordion from "./Accordion"
-import { questions } from "./data.ts"
-import bg from "../../../public/faq-bg.png"
+// import Accordion from "./Accordion"
+// import { questions } from "./data.ts"
 
 const Faq = () => {
   return (
     <section id='faq' className="py-32 overflow-hidden w-full ">
       <div className="container space-y-4 mx-auto px-4 md:px-6 relative">
-        <div className="bg-foreground p-8 rounded-2xl bg-[url('/faq-bg.png')] bg-no-repeat bg-bottom-right bg-size-[380px]">
+        <div className="bg-foreground p-8 rounded-2xl bg-[url('/faq-bg.png')] bg-no-repeat bg-bottom-right bg-size-[375px]">
           <h2 className="text-2xl xl:text-3xl mb-4 font-bold uppercase text-primary tracking-wide leading-none">Frequently asked questions</h2>
           <p className="max-w-130 text-lg text-secondary font-medium leading-normal">Have more questions? Reach out to our team or submit an estimation request.</p>
           <button className='absolute top-[58%] left-[57%] text-white text-sm font-bold bg-primary rounded-full py-10 px-2 border-12 border-white cursor-pointer'>Get Estimate</button>
