b0f44a2892b9ffd3b07e2234e0a51494c528046e
Ruben Aguilar <ragui56@wgu.edu>
2026-08-23 23:25:11 -0700

    update styling for faq header

--- client/src/sections/faq/Faq.tsx
+++ client/src/sections/faq/Faq.tsx
@@ -3,16 +3,18 @@
 
 const Faq = () => {
   return (
-    <section id='faq' className="py-32 overflow-hidden w-full ">
+    <section id='faq' className="py-32 overflow-hidden w-full">
       <div className="container space-y-4 mx-auto px-4 md:px-6 relative">
-        <div className="bg-foreground p-8 rounded-2xl bg-[url('/faq-bg.png')] bg-no-repeat bg-bottom-right">
-          <h2 className="text-2xl xl:text-3xl mb-4 font-bold uppercase text-primary tracking-wide leading-none">
-            Frequently asked questions
-          </h2>
-          <p className="max-w-130 text-lg text-secondary font-medium leading-normal">
-            Have more questions? Reach out to our team or submit an estimation request.
-          </p>
-          <button className='absolute top-[58%] left-[52%] text-white text-sm font-bold bg-primary rounded-full :py-10 px-2 xl:py-12 xl:px-4 border-14 border-white cursor-pointer'>
+        <div className="bg-foreground p-8 rounded-2xl lg:bg-[url('/faq-bg.png')] bg-no-repeat bg-bottom-right">
+          <div className="py-5">
+            <h2 className="text-xl xl:text-3xl mb-4 font-bold uppercase text-primary tracking-wide leading-none">
+              Frequently asked questions
+            </h2>
+            <p className="max-w-130 lg:text-lg text-secondary font-medium leading-normal">
+              Have more questions? Reach out to our team or submit an estimation request.
+            </p>
+          </div>
+          <button className='absolute top-[66%] md:top-[58%] left-[65%] text-white text-xs lg:text-sm font-bold bg-primary rounded-full py-10 px-2 xl:py-12 xl:px-4 border-12 md:border-14 border-white cursor-pointer'>
             Get Estimate
           </button>
         </div>
