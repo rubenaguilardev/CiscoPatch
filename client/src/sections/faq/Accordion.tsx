9f9b3943582234030934709593cd797556993dcd
Ruben Aguilar <ragui56@wgu.edu>
2026-08-28 08:44:52 -0700

    improve faq button padding

--- client/src/sections/faq/Accordion.tsx
+++ client/src/sections/faq/Accordion.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import type { FaqItem } from "./data"
 
 
-const Accordion = ({question, answer, category, id}: FaqItem) => {
+const Accordion = ({ question, answer, category, id }: FaqItem) => {
 
   const [accordionOpen, setAccordionOpen] = useState(false)
 
@@ -10,49 +10,47 @@ const Accordion = ({question, answer, category, id}: FaqItem) => {
   const isLastInCategory = id === `${category}4`
 
   return (
-  
-  <div className="hover:text-primary transition-colors duration-150">
-    <button onClick={() => setAccordionOpen(!accordionOpen)} className={`w-full flex justify-between cursor-pointer ${accordionOpen ? "bg-[#4D2C24] text-primary font-bold" : ""}`}>
-      <div className={`flex justify-between w-full mx-8 ${accordionOpen ? "border-none" : "border-b border-muted"} ${isFirstInCategory ? "pt-3" : "pt-8"} ${isLastInCategory ? "border-none pb-0" : "pb-6"}`}>
-      <span className="">{question}</span>
-      <div className="flex items-center justify-center p-1 rounded-full border text-primary">
-        <svg
-          className="shrink-0"
-          width="16"
-          height="16"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect
-            y="7"
-            width="16"
-            height="2"
-            rx="1"
-            fill="currentColor"
-            className={`transform origin-center transition duration-200 ease-out ${
-              accordionOpen && "rotate-180"
-            }`}
-          />
-          <rect
-            y="7"
-            width="16"
-            height="2"
-            rx="1"
-            fill="currentColor"
-            className={`transform origin-center rotate-90 transition duration-200 ease-out ${
-              accordionOpen && "rotate-180"
-            }`}
-          />
-        </svg>
-      </div>
-      </div>
-    </button>
-    
-    <div className={`grid overflow-hidden transitiona-all duraction-300 ease-in-out text-sm bg-[#4D2C24]
+
+    <div className="hover:text-primary transition-colors duration-150">
+      <button onClick={() => setAccordionOpen(!accordionOpen)} className={`w-full flex justify-between cursor-pointer ${accordionOpen ? "bg-[#4D2C24] text-primary font-bold" : ""}`}>
+        <div className={`flex justify-between w-full mx-8 ${accordionOpen ? "border-none" : "border-b border-muted"} ${isFirstInCategory ? "pt-3" : "pt-8"} ${isLastInCategory ? "border-none pb-0" : "pb-6"}`}>
+          <span className="">{question}</span>
+          <div className="flex items-center justify-center p-1 rounded-full border text-primary">
+            <svg
+              className="shrink-0"
+              width="16"
+              height="16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                y="7"
+                width="16"
+                height="2"
+                rx="1"
+                fill="currentColor"
+                className={`transform origin-center transition duration-200 ease-out ${accordionOpen && "rotate-180"
+                  }`}
+              />
+              <rect
+                y="7"
+                width="16"
+                height="2"
+                rx="1"
+                fill="currentColor"
+                className={`transform origin-center rotate-90 transition duration-200 ease-out ${accordionOpen && "rotate-180"
+                  }`}
+              />
+            </svg>
+          </div>
+        </div>
+      </button>
+
+      <div className={`grid overflow-hidden transitiona-all duraction-300 ease-in-out text-sm bg-[#4D2C24]
       ${accordionOpen ? "grid-rows[1fr] opacity-100" : "grid-rows[0fr] opacity-0"}
     `}>
-      <div className={`${accordionOpen ? 'max-w-3xl flex py-6 px-8 text-secondary font-medium' : 'hidden'}`}>{answer}</div>
+        <div className={`${accordionOpen ? 'max-w-3xl flex pt-2 pb-6 px-8 text-secondary font-medium' : 'hidden'}`}>{answer}</div>
+      </div>
     </div>
-  </div>
   )
 }
 
--- client/src/sections/faq/Faq.tsx
+++ client/src/sections/faq/Faq.tsx
@@ -17,47 +17,45 @@ const Faq = () => {
             <h2 className="text-xl xl:text-3xl font-bold uppercase text-primary tracking-wide leading-none">
               Frequently asked questions
             </h2>
-            <p className="max-w-130 lg:text-lg text-secondary font-medium leading-normal">
+            <p className="max-w-130 lg:text-lg pb-8 text-secondary font-medium leading-normal">
               Have more questions? Reach out to our team or submit an estimation request.
             </p>
           </div>
-          <button className='absolute top-[66%] md:top-[58%] left-[65%] md:left-[55%] text-white text-xs lg:text-sm font-bold bg-primary rounded-full py-10 px-2 xl:py-12 xl:px-4 border-12 md:border-14 border-white cursor-pointer'>
+          <button className='absolute top-[75%] md:top-[58%] left-[54%] md:left-[55%] text-white text-xs lg:text-sm font-bold bg-primary rounded-full py-10 px-2 xl:py-12 xl:px-4 border-12 md:border-14 border-white cursor-pointer'>
             Get Estimate
           </button>
         </div>
 
-        
-    
-        
-        </div>
-        <div className="flex justify-center text-center gap-0.75 mt-32">
-          {labels.map((label, index) => (
-            <div 
-              key={index}
-              onClick={() => setActiveLabel(label)} 
-              className={`w-36 h-13 flex justify-center items-center rounded-t-2xl text-lg font-bold ${label === activeLabel ? "bg-foreground text-primary" : "text-secondary hover:bg-[#4D2C24]/80"} transition-colors duration-250 ease-in-out cursor-pointer`}
-            >
-              {label}
-            </div>
-          ))}
-        
-        </div>
-          
-          <div className="max-w-206 mx-auto py-8 flex flex-col text-white bg-foreground rounded-2xl">
-            <div className="flex items-center gap-3 mb-8 px-8">
-              <CircleDollarSign className="w-8 h-8 text-primary"/>
-              <div>
-                <h3 className="text-lg font-bold ">Cost</h3>
-                <p className="font-bold text-sm text-secondary">Pricing and estimates</p>
-              </div>
-            </div>
-            {questions.map(({question, answer, category, id}, index) => (
-          <Accordion key={index} question={question} answer={answer} category={category} id={id}/>
-        ))} 
+
+      </div>
+      <div className="flex justify-center text-center gap-0.75 mt-32">
+        {labels.map((label, index) => (
+          <div
+            key={index}
+            onClick={() => setActiveLabel(label)}
+            className={`w-36 h-13 flex justify-center items-center rounded-t-2xl text-lg font-bold ${label === activeLabel ? "bg-foreground text-primary" : "text-secondary hover:bg-[#4D2C24]/80"} transition-colors duration-250 ease-in-out cursor-pointer`}
+          >
+            {label}
           </div>
+        ))}
+
+      </div>
+
+      <div className="max-w-206 mx-auto py-8 flex flex-col text-white bg-foreground rounded-2xl">
+        <div className="flex items-center gap-3 mb-8 px-8">
+          <CircleDollarSign className="w-8 h-8 text-primary" />
+          <div>
+            <h3 className="text-lg font-bold ">Cost</h3>
+            <p className="font-bold text-sm text-secondary">Pricing and estimates</p>
+          </div>
+        </div>
+        {questions.map(({ question, answer, category, id }, index) => (
+          <Accordion key={index} question={question} answer={answer} category={category} id={id} />
+        ))}
+      </div>
     </section>
-      
-      
+
+
   )
 }
 
