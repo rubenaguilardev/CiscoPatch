10e21e9336504400c5e603d1db83b48084a120dc
Ruben Aguilar <ragui56@wgu.edu>
2026-09-08 23:49:40 -0700

    add inputs

--- client/src/sections/Contact.tsx
+++ client/src/sections/Contact.tsx
@@ -2,11 +2,39 @@ const Contact = () => {
   return (
     <section id="contact">
       <div>
-        <div>
-          <h1>Contact</h1>
-        </div>
-      </div>
-    </section>
+        <form action="" className="flex flex-col">
+          <div className="flex">
+            <div className="flex flex-col">
+              <label htmlFor="firstName">First Name</label>
+              <input id="firstName" type="text" />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="lastName">Last Name</label>
+              <input id="lastName" type="text" />
+            </div>
+          </div>
+          <div>
+            <p>Address Information</p>
+            <label htmlFor="address">Address</label>
+            <input id="address" type="text" />
+            <div className="flex">
+              <div className="flex flex-col">
+                <label htmlFor="city">City</label>
+                <input id="city" type="text" />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="state">State</label>
+                <input id="state" type="text" />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="zip">ZIP</label>
+                <input id="zip" type="text" />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div >
+    </section >
   )
 }
 
