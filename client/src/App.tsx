a0ac245530699001e4ad3d4e198023d727f2d908
rubenaguilardev <ragui56@wgu.edu>
2026-09-19 23:50:27 -0700

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import Services from "./sections/Services"
 import About from "./sections/About"
 import Faq from "./sections/faq/Faq"
 import Results from "./sections/Results"
-import Contact from "./sections/Contact"
+import Contact from "./Contact"
 import Footer from "./sections/Footer"
 
 
--- client/src/Contact.tsx
+++ client/src/Contact.tsx
@@ -1,6 +1,6 @@
-import { Mail, Phone, MapPin, type LucideIcon } from "lucide-react"
+import { Mail, Phone, MapPin, ArrowUpRight, type LucideIcon } from "lucide-react"
 
-type contactInfo = {
+export type contactInfo = {
   icon: LucideIcon,
   label: string,
   info: string
@@ -15,22 +15,25 @@ const contacts: contactInfo[] = [
 const Contact = () => {
   return (
     <section id="contact" className="py-32 overflow-hidden">
-      <div>
+      <div className="container mx-auto px-4 md:px-6 pt-32 pb-4 md:pb-8">
         <div>
           <div>
-            <h2>Get a free estimate</h2>
+            <h2 className="text-[2rem] font-bold">Get a free estimate</h2>
             <div>
               <div className="grid gap-4">
                 {contacts.map(({icon: Icon, label, info}) => (
-                  <div key={label} className="">
+                  <div key={label} className="flex justify-between items-center border border-light-border rounded-2xl p-4">
                     <div className="flex items-center gap-4">
-                      <div>
-                        {<Icon />}
+                      <div className="rounded-full p-2.5 border border-light-border ">
+                      {<Icon  className="h-6"/>}
                       </div>
                       <div className="">
-                        <span>{label}</span>
-                        <p>{info}</p>
+                      <span className="roboto font-bold">{label}</span>
+                      <p className="text-sm">{info}</p>
                       </div>
+                    </div> 
+                    <div className="flex items-center justify-center h-8 w-8 rounded-full bg-black">
+                    <ArrowUpRight className="text-white h-5 w-5"/>
                     </div>
                   </div>
                 ))}
@@ -38,6 +41,7 @@ const Contact = () => {
             </div>
           </div>
         </div>
+        <div></div>
       </div>
     </section >
   )
