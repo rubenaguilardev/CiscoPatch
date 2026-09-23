ad27eacabe4da3c7711efc68438a01ee881a0543
rubenaguilardev <ragui56@wgu.edu>
2026-09-23 00:13:55 -0700

    add inputs to form

--- client/src/Contact.tsx
+++ client/src/Contact.tsx
@@ -1,4 +1,5 @@
 import { Mail, Phone, MapPin, ArrowUpRight, type LucideIcon } from "lucide-react"
+import Button from "./components/Button"
 
 type contactInfo = {
   icon: LucideIcon,
@@ -22,19 +23,25 @@ type Input = {
 const inputs: Input[] = [
   { id: "firstName", label: "First Name", type: "text", placeholder: "John"},
   { id: "lastName", label: "Last Name", type: "text", placeholder: "Doe"},
+  { id: "email", label: "Email", type: "email", placeholder: "johndoe@email.com"},
+  { id: "mobile", label: "Mobile", type: "text", placeholder: "(909)951-9282"},
+  { id: "address", label: "Address", type: "text", placeholder: "123 Main Ct"},
+  { id: "city", label: "City", type: "text", placeholder: "Riverside"},
+  { id: "zip", label: "ZIP", type: "text", placeholder: "92225"},
+  
 ]
 
 const Contact = () => {
   return (
-    <section id="contact" className="py-32 overflow-hidden">
+    <section id="contact" className="overflow-hidden">
       <div className="container mx-auto px-4 md:px-6 pt-32 pb-4 md:pb-8">
-        <div className="grid grid-cols-2">
+        <div className="grid grid-cols-2 gap-8">
           <div>
             <h2 className="text-[2rem] font-bold">Get a free estimate</h2>
             <div>
               <div className="grid gap-4">
                 {contacts.map(({icon: Icon, label, info}) => (
-                  <div key={label} className="flex justify-between items-center border border-light-border rounded-2xl p-4">
+                  <div key={label} className="flex justify-between items-center bg-[#f5f5f5]  rounded-2xl p-4">
                     <div className="flex items-center gap-4">
                       <div className="rounded-full p-2.5 border border-light-border ">
                       {<Icon  className="h-6"/>}
@@ -54,19 +61,33 @@ const Contact = () => {
           </div>
      
           <div>
-          <form>
-            <div className="flex">
-            {inputs.map(({id, label, type, placeholder}) => (
-              <div key={id} className="flex gap-4">
-                <div className="flex flex-col">
-                  <label htmlFor={id} />{label}
-                  <input type={type} id={id} name={id} placeholder={placeholder}  />
+          <form className="space-y-8">
+            <div className="grid grid-cols-2 gap-8">
+              {inputs.map(({id, label, type, placeholder}) => (
+                <div key={id} className={`${id === "address" ? "col-span-2" : " "}`}>
+                  <div className="flex flex-col">
+                    <label htmlFor={id}>{label}</label>
+                    <input type={type} id={id} name={id} placeholder={placeholder} />
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
             </div>
-            
-          
+            <div className="flex flex-col">
+              <label htmlFor="description">Description</label>
+              <textarea 
+                id="description" 
+                name="description" 
+                rows={5} 
+                className="border"
+                placeholder="Description of work..."
+              >
+              </textarea>
+            </div>
+             <div className="flex flex-col">
+              <label htmlFor="attach">Attach</label>
+              <input type="file" id="attach" name="attach" className="border py-10" />
+            </div>
+            <Button className="w-full">Submit</Button>
           </form>
           </div>
         </div>
